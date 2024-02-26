d971142010a7d55c0ad57b67b3060dfa9957d5bb
Rodrigo Esprella <hairaxelpoderoso@gmail.com>
2024-02-26 10:20:01 -0400

    complete excercise

--- 26/02/patterMaching.hs
+++ 26/02/patterMaching.hs
@@ -3,11 +3,6 @@ module SepCadena where
     split :: String -> Char ->[String]
     split "" _ =[]
     split (x:xs) c | x == c = split xs c
-                   | otherwise = (x : head (split xs c)) : tail (split xs c) 
-
-    splitP :: String -> [String]
-    splitP "" = []
-    splitP xs = ys : (splitP . drop 1) zs
-        where (ys, zs) = span (/=' ') xs
-
-
+                   | otherwise = (x : 
+                        takeWhile (/= c) xs ) : 
+                            split (dropWhile(/=c)xs) c
